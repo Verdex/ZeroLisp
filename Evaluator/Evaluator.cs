b06f2bb43f5c2e396d356c0e633265a22f0cf995
David L. Inman <davidlevere@gmail.com>
2018-02-11 00:13:47 -0500

    evaluator inc prog

--- Evaluator/Evaluator.cs
+++ Evaluator/Evaluator.cs
@@ -1,7 +1,16 @@
 
+using ZeroLisp.Data;
+using ZeroLisp.Reader;
+
+using static ZeroLisp.Data.Option;
+using static ZeroLisp.Reader.Reader;
+
 namespace ZeroLisp.Evaluator
 {
+    // TODO this will need some sort of runtime info
+    // symbol table (global, module, local?)
     public class Evaluator
     {
+
     }
 }
